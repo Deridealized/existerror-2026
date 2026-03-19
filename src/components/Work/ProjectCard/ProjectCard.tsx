4588b92cc22f10b954a7d2f557daddb97ece2920
Deridealized <jamie.hey@sky.com>
2026-03-19 15:23:18 +0000

    fix(multi): removed grain fx, improve featured card

--- src/components/Work/ProjectCard/ProjectCard.tsx
+++ src/components/Work/ProjectCard/ProjectCard.tsx
@@ -25,37 +25,44 @@ const ProjectCard = ({
 }: ProjectCardProps) => {
   return (
     <div className={s.projectCard + (featured ? " " + s.featured : "")}>
-      <div className={s.projectType}>{type}</div>
-      <div className={s.projectName}>
-        {projectName}
-        <div className={s.projectSubtitle}>{subtitle}</div>
-      </div>
+      <div>
+        {featured && <div className={s.featuredBadge}>Commercial Release</div>}
 
-      <p className={s.projectDesc}>{description}</p>
-      <img
-        src={image}
-        alt={`${projectName} screenshot`}
-        className={s.projectImage}
-      />
-      <div className={s.projectStack}>
-        {stack.map((tech, index) => (
-          <span key={index} className={s.tag}>
-            {tech}
-          </span>
-        ))}
+        <div className={s.projectType}>{type}</div>
+        <div className={s.projectName}>
+          {projectName}
+          <div className={s.projectSubtitle}>{subtitle}</div>
+        </div>
+
+        <p className={s.projectDesc}>{description}</p>
+
+        <div className={s.projectStack}>
+          {stack.map((tech, index) => (
+            <span key={index} className={s.tag}>
+              {tech}
+            </span>
+          ))}
+        </div>
       </div>
-      <a
-        href={link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className={s.projectLink}
-      >
-        {linkIcon ? (
-          <img src={linkIcon} alt="Link Icon" className={s.linkIcon} />
-        ) : (
-          "↗"
-        )}
-      </a>
+      <div className={featured ? s.featuredVisual : ""}>
+        <img
+          src={image}
+          alt={`${projectName} screenshot`}
+          className={s.projectImage}
+        />
+        <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={s.projectArrow}
+        >
+          {linkIcon ? (
+            <img src={linkIcon} alt="Link Icon" className={s.linkIcon} />
+          ) : (
+            "↗"
+          )}
+        </a>
+      </div>
     </div>
   );
 };
