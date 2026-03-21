6a606f5c75fb44189bda3eceeda89522568aa19c
Deridealized <jamie.hey@sky.com>
2026-03-21 21:16:11 +0000

    feat(router): add helloworld page and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Error from "./pages/Error/Error";
+import HelloWorld from "./pages/HelloWorld/HelloWorld";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="*" element={<Error />} />
+      <Route path="/helloworld" element={<HelloWorld />} />
     </Routes>
   );
 }
