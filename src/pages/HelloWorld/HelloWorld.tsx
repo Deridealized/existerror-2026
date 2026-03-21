a41ca442604597e00f4ee7feab3ed3662d7ee738
Deridealized <jamie.hey@sky.com>
2026-03-21 21:17:54 +0000

    feat(page): helloworld return

--- src/pages/HelloWorld/HelloWorld.tsx
+++ src/pages/HelloWorld/HelloWorld.tsx
@@ -1,5 +1,14 @@
+import s from "./HelloWorld.module.css";
+
 const HelloWorld = () => {
-  return <div>HelloWorld</div>;
+  return (
+    <div>
+      HelloWorld
+      <a href="/" className={s.homeBtn}>
+        ← Return to safety - there's nothing for you here.
+      </a>
+    </div>
+  );
 };
 
 export default HelloWorld;
