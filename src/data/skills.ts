bafd237d690a4b53e0609341505ecd7ef1ae28c5
Deridealized <jamie.hey@sky.com>
2026-03-24 09:22:24 +0000

    update(main): pull latest and update skills

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -9,36 +9,36 @@ export const skills: Skill[] = [
   { name: "React", category: "Frontend", level: 5 },
   { name: "TypeScript", category: "Frontend", level: 4 },
   { name: "C#", category: "Backend", level: 5 },
-  { name: "JavaScript", category: "Frontend", level: 5 },
-  { name: "HTML/CSS", category: "Frontend", level: 5 },
+  { name: "JavaScript", category: "Frontend", level: 4 },
+  { name: "HTML/CSS", category: "Frontend", level: 4 },
 
   // BE
   { name: "SQL", category: "Backend", level: 2 },
   { name: "GraphQL", category: "Backend", level: 2 },
 
   // Engineering Principles
-  { name: "SOLID Principles", category: "Engineering", level: 4 },
+  { name: "SOLID Principles", category: "Engineering", level: 3 },
   { name: "Design Patterns", category: "Engineering", level: 3 },
   { name: "Unit Testing", category: "Engineering", level: 3 },
   { name: "Agile ", category: "Engineering", level: 5 },
 
   // Tooling
-  { name: "Git", category: "Tooling", level: 4 },
-  { name: "Vitest", category: "Tooling", level: 3 },
+  { name: "Git", category: "Tooling", level: 5 },
+  { name: "Vitest", category: "Tooling", level: 2 },
   { name: "Figma", category: "Tooling", level: 3 },
   { name: "Postman", category: "Tooling", level: 2 },
   { name: "AI", category: "Tooling", level: 4 },
 
   // Communication
-  { name: "Jira", category: "Communication", level: 3 },
-  { name: "Miro", category: "Communication", level: 3 },
+  { name: "Jira", category: "Communication", level: 4 },
+  { name: "Miro", category: "Communication", level: 4 },
 
   // Creative
-  { name: "Unity", category: "Creative", level: 3 },
+  { name: "Unity", category: "Creative", level: 5 },
   { name: "Photoshop", category: "Creative", level: 5 },
-  { name: "Illustrator", category: "Creative", level: 3 },
+  { name: "Illustrator", category: "Creative", level: 4 },
   { name: "After Effects", category: "Creative", level: 4 },
-  { name: "Premiere Pro", category: "Creative", level: 2 },
-  { name: "Blender", category: "Creative", level: 3 },
+  { name: "Premiere Pro", category: "Creative", level: 1 },
+  { name: "Blender", category: "Creative", level: 2 },
   { name: "FL Studio", category: "Creative", level: 2 },
 ];
